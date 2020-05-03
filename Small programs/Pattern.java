1038c2f0e8a3a2441ee166ee7204c6012a06e8eb
Avinal <avinal.xlvii@gmail.com>
2020-05-03 17:54:20 +0530

    formatting fixed

--- Small programs/Pattern.java
+++ Small programs/Pattern.java
@@ -1,9 +1,7 @@
 public class Pattern {
     public static void main(String Args[]) {
         int i, j;
-        for (i = 1; i <= 5; ++i)
-
-        {
+        for (i = 1; i <= 5; ++i) {
             System.out.println();
             for (j = 1; j <= i; ++j)
                 System.out.print("*");
