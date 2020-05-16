ee35ea84087a6a44687975685bdb8d4f3ab4894f
Kritika Srivastava <kritika.s1408@gmail.com>
2020-05-16 20:02:13 +0530

    Add files via upload

---
+++ Geeks for Geeks/stack.java
@@ -0,0 +1,52 @@
+
+//A program to solve a postfix stack notation.
+import java.util.*;
+import java.util.Scanner;
+
+class stack {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int t = kb.nextInt();
+		for (int j = 0; j < t; j++) {
+			String m = kb.next();
+			int n, l;
+			Stack<Integer> st = new Stack<Integer>();
+			for (int i = 0; i < m.length(); i++) {
+				if (Character.isDigit(m.charAt(i)) == true) {
+					int a = Character.getNumericValue(m.charAt(i));
+					st.push(a);
+				} else {
+					char k = m.charAt(i);
+					if (st.isEmpty() == false) {
+						if (k == '+') {
+							l = st.pop();
+							n = st.pop();
+							st.push(n + l);
+
+						} else if (k == '-') {
+							l = st.pop();
+							n = st.pop();
+							st.push(n - l);
+
+						} else if (k == '*') {
+							l = st.pop();
+							n = st.pop();
+							st.push(n * l);
+
+						} else if (k == '/') {
+							l = st.pop();
+							n = st.pop();
+							st.push(n / l);
+
+						}
+					} else {
+					}
+				}
+
+			}
+			System.out.println(st.pop());
+
+		}
+		kb.close();
+	}
+}
