2011dac363568c3acc4bb6060b95b395159d816a
Kritika Srivastava <kritika.s1408@gmail.com>
2020-06-08 22:38:01 +0530

    Create JumpingOnClouds.java

---
+++ Hackerrank/JumpingOnClouds.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class JumpingOnClouds {
+
+    static int jumpingOnClouds(int[] c) {int k=0;
+     for (int i=1;i<c.length;i++)
+     {if(c[i]==0){k++;}
+     else{}
+     }
+    for (int i=0;i<c.length-2;i++)
+     {if((c[i]==0)&&(c[i+1]==0)&&(c[i+2]==0)){k--;i++;}
+     else{}
+     } 
+     return k;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
