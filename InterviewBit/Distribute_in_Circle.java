222a2804a78ec64a717e099b8df9596268a3bdd7
Kritika Srivastava <kritika.s1408@gmail.com>
2021-02-09 22:25:52 +0530

    Create Distribute_in_Circle.java

---
+++ InterviewBit/Distribute_in_Circle.java
@@ -0,0 +1,18 @@
+
+public class Distribute_in_Circle {
+    public int solve(int A, int B, int C) {
+        if(A+C<=B)
+        {
+            return A+C-1;
+        }
+        else
+        {
+            int m=A+C;
+            while(m>B)
+            {
+                m-=B;
+            }
+           return m-1;
+        }
+    }
+}
