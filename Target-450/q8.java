ac66a6263b6e7124e7c7b65008a1017befafff67
Kritika Srivastava <kritika.s1408@gmail.com>
2021-03-03 22:49:20 +0530

    Questions

---
+++ Target-450/q8.java
@@ -0,0 +1,62 @@
+
+/**
+ * /mnt/e/#Fod Do/Target 450/Codes/q8.java
+ * @author Kritika Srivastava
+ * @since March 03, 2021
+ *
+ * Find Largest sum contiguous Subarray 
+ * Link - https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+ */
+import java.io.*;
+
+class q8 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            // size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // adding elements
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Kadane obj = new Kadane();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Kadane {
+
+    // Function to find subarray with maximum sum
+    // arr: input array
+    // n: size of array
+    int maxSubarraySum(int arr[], int n) {
+        int sum = 0, max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (sum > max) {
+                max = sum;
+            } else {
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+
+        }
+        return max;
+
+    }
+
+}
