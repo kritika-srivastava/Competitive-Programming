b43ee8430d1154100ec5468609078f15f6360f0e
Kritika Srivastava <kritika.s1408@gmail.com>
2021-03-18 21:09:27 +0530

    158A

--- Codeforces/M_arrays.java
+++ Codeforces/M_arrays.java
@@ -1,28 +1,32 @@
+/**
+ * /mnt/e/#Fod Do/Codeforces/Next_Round.java
+ * @author Kritika Srivastava
+ * @since March 18, 2021
+ *
+ * Link - https://codeforces.com/problemset/problem/158/A
+ * Comments - Took me 5 minutes to write and 45 minutes to debug.
+ * I seriously dont know why though.
+ */
 import java.util.*;
-public class M_arrays {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int t = kb.nextInt();
-        while (t-- > 0) {
-            int n = kb.nextInt();
-            int m=kb.nextInt();
-            int a[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = kb.nextInt();
-            }
-            Arrays.sort(a);
-            int sum=a[0];
-            for(int i=1;i<n;i++)
-            {
-                if(sum+a[i]%m==0)
-                {}
-                else{
-                    
-                }
-            {
 
-            }
+public class Next_Round {
+    public static void main(String[] Args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt() - 1;
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        int out = 0;
+        int m = a[k];
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= m && a[i] != 0) {
+                out++;
+            } else {
+                break;
             }
         }
+        System.out.println(out);
     }
 }
