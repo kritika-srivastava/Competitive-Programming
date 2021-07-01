b82c97519f3d51cd46cdc120601956b537a67c43
kritika-srivastava <kritika.s1408@gmail.com>
2021-07-01 10:18:47 +0530

    Anagrams using backtracking

--- Part-2/anagram.java
+++ Part-2/anagram.java
@@ -1,34 +1,39 @@
 
-/*PROGRAM 6
- Write a program to enter a word and generate anagrams of the word.
- */
-import java.util.Scanner;
+// anagrams using backtracking - completely my imagination...duh
+// but heyyy, it works...
+import java.util.*;
 
 public class anagram {
-    String a = " ";
-    static int f = 0;
-
-    void anagram(String prefix, String a) {
-        if (a.length() <= 1) {
-            System.out.println(prefix + a);
-            f++;
-        } else {
-            for (int i = 0; i < a.length(); i++) {
-                String cur = a.substring(i, i + 1);
-                String before = a.substring(0, i);
-                String after = a.substring(i + 1);
-                anagram(prefix + cur, before + after);
-            }
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String s = kb.nextLine();
+        LinkedList<String> l = anagram(s);
+        for(String i:l)
+        {
+            System.out.println(i);
         }
     }
 
-    public static void main(String Args[]) {
-        Scanner kb = new Scanner(System.in);
-        anagram ob = new anagram();
-        System.out.println("Enter the string;");
-        String a = kb.nextLine();
-        String b = " ";
-        ob.anagram(b, a);
-        System.out.println("Frequency = " + f);
+    static LinkedList<String> anagram(String s) {
+        LinkedList<String> l = new LinkedList<String>();
+        boolean[] visited = new boolean[s.length()];
+        backtrack(l, visited, s, "");
+        return l;
+    }
+
+    static void backtrack(LinkedList<String> l, boolean[] visited, String s, String st) {
+        if (st.length() == s.length() && !l.contains(st)) {
+            l.add(st);
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                if (visited[i] == false) {
+                    st += s.charAt(i);
+                    visited[i] = true;
+                    backtrack(l, visited, s, st);
+                    visited[i] = false;
+                    st = st.substring(0, st.length()-1);
+                }
+            }
+        }
     }
 }
